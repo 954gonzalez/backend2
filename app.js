6375737b79e4cc83a1fa16ea4599b5c2787e8027
954gonzalez <jagonzalez954@soy.sena.edu.co>
2023-09-07 14:50:01 -0500

    back

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const login = require("./routes/login");
+
+const PORT = 10101;
+
+const app = express()
+  .use(
+    cors({
+      credentials: true,
+      origin: "*",
+    })
+  )
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/login", login);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor express ejecutado en el puerto ${PORT}`);
+});
